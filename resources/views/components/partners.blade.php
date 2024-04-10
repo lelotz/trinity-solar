45dddf29bb257f869975f70548fc8669530839e9
lelotz <olomidereck@hotmail.com>
2024-04-10 10:12:49 +0300

    Last commit before pushing to remote repo

---
+++ resources/views/components/partners.blade.php
@@ -0,0 +1,29 @@
+
+<section class="bg-light py-5 py-xl-6">
+  <div class="container mb-5 mb-md-6">
+    <div class="row justify-content-md-center">
+      <div class="col-12 col-md-10 col-lg-8 col-xl-7 col-xxl-6 text-center">
+        <h2 class="mb-4 display-5">Clients</h2>
+        <p class="text-secondary mb-4 mb-md-5">Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque et neque id ligula mattis commodo.</p>
+        <hr class="w-50 mx-auto mb-0 text-secondary">
+      </div>
+    </div>
+  </div>
+  <div class="container overflow-hidden">
+    <div class="row gy-5 gy-md-6">
+        <div class="col-6 col-md-3 align-self-center text-center">
+            <img class="img-fluid" src="images/imed-logo-c.png" alt="" width="225" height="130" viewBox="0 0 225 130" fill="currentColor" class="text-dark"></img>
+        </div>
+        <div class="col-6 col-md-3 align-self-center text-center">
+            <img class="img-fluid" src="images/un-logo-c.png" alt="" width="225" height="130" viewBox="0 0 225 130" fill="currentColor" class="text-dark"></img>
+        </div>
+        <div class="col-6 col-md-3 align-self-center text-center">
+            <img class="img-fluid" src="images/whitemark_logo-c.png" alt="" width="225" height="130" viewBox="0 0 225 130" fill="currentColor" class="text-dark"></img>
+        </div>
+        <div class="col-6 col-md-3 align-self-center text-center">
+            <img class="img-fluid" src="images/anza-logo-c.png" alt="" width="225" height="130" viewBox="0 0 225 130" fill="currentColor" class="text-dark"></img>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
