e52ba912c5c236204e93be9b3555a1889c428c6f
lelotz <olomidereck@hotmail.com>
2024-04-11 08:47:51 +0300

    encapsulated article component

--- resources/views/components/service.blade.php
+++ resources/views/components/service.blade.php
@@ -5,85 +5,51 @@
             <h6 class="text-primary">Our Services</h6>
             <h1 class="mb-4">We Are Pioneers In The World Of Renewable Energy</h1>
         </div>
-        <div class="row g-4">
-            <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.1s">
-                <div class="service-item rounded overflow-hidden">
-                    <img class="img-fluid" src="images/b-gallery-1.jpg" alt="">
-                    <div class="position-relative p-4 pt-0">
-                        <div class="service-icon">
-                            <i class="fa fa-solar-panel fa-3x"></i>
-                        </div>
-                        <h4 class="mb-3">Solar Panels</h4>
-                        <p>Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam lorem diam.</p>
-                        <a class="small fw-medium" href="">Read More<i class="fa fa-arrow-right ms-2"></i></a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.3s">
-                <div class="service-item rounded overflow-hidden">
-                    <img class="img-fluid" src="images/b-gallery-2.jpg" alt="">
-                    <div class="position-relative p-4 pt-0">
-                        <div class="service-icon">
-                            <i class="fa fa-wind fa-3x"></i>
-                        </div>
-                        <h4 class="mb-3">Wind Turbines</h4>
-                        <p>Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam lorem diam.</p>
-                        <a class="small fw-medium" href="">Read More<i class="fa fa-arrow-right ms-2"></i></a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.5s">
-                <div class="service-item rounded overflow-hidden">
-                    <img class="img-fluid" src="images/b-gallery-3.jpg" alt="">
-                    <div class="position-relative p-4 pt-0">
-                        <div class="service-icon">
-                            <i class="fa fa-lightbulb fa-3x"></i>
-                        </div>
-                        <h4 class="mb-3">Hydropower Plants</h4>
-                        <p>Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam lorem diam.</p>
-                        <a class="small fw-medium" href="">Read More<i class="fa fa-arrow-right ms-2"></i></a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.1s">
-                <div class="service-item rounded overflow-hidden">
-                    <img class="img-fluid" src="images/b-gallery-4.jpg" alt="">
-                    <div class="position-relative p-4 pt-0">
-                        <div class="service-icon">
-                            <i class="fa fa-solar-panel fa-3x"></i>
-                        </div>
-                        <h4 class="mb-3">Solar Panels</h4>
-                        <p>Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam lorem diam.</p>
-                        <a class="small fw-medium" href="">Read More<i class="fa fa-arrow-right ms-2"></i></a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.3s">
-                <div class="service-item rounded overflow-hidden">
-                    <img class="img-fluid" src="images/b-gallery-5.jpg" alt="">
-                    <div class="position-relative p-4 pt-0">
-                        <div class="service-icon">
-                            <i class="fa fa-wind fa-3x"></i>
-                        </div>
-                        <h4 class="mb-3">Wind Turbines</h4>
-                        <p>Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam lorem diam.</p>
-                        <a class="small fw-medium" href="">Read More<i class="fa fa-arrow-right ms-2"></i></a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.5s">
-                <div class="service-item rounded overflow-hidden">
-                    <img class="img-fluid" src="images/b-gallery-6.jpg" alt="">
-                    <div class="position-relative p-4 pt-0">
-                        <div class="service-icon">
-                            <i class="fa fa-lightbulb fa-3x"></i>
-                        </div>
-                        <h4 class="mb-3">Hydropower Plants</h4>
-                        <p>Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam lorem diam.</p>
-                        <a class="small fw-medium" href="">Read More<i class="fa fa-arrow-right ms-2"></i></a>
-                    </div>
-                </div>
-            </div>
+        <div class="row g-4" style="justify-content:center">
+
+            @include('widgets.artical_card',[
+                'title'=>'Solar Panel Installation',
+                'img'=>'images/b-gallery-1.jpg',
+                'description'=>'We meticulously design and implement customized solar solutions tailored to your energy needs.',
+                'href'=>''
+                ])
+            
+            @include('widgets.artical_card',
+            [
+                'title'=>'Solar Components Supplier',
+                'img'=>'images/b-gallery-2.jpg',
+                'description'=>'Energize your projects with our comprehensive Supply of Solar Components service.',
+                'href'=>''
+            ])
+
+            @include('widgets.artical_card',
+            [
+                'title'=>'Photo Voltaic Systems',
+                'img'=>'images/b-gallery-3.jpg',
+                'description'=>'Expert integration of diverse solar technologies, including backup systems, streetlights, and advanced hybrids',
+                'href'=>''
+            ])
+
+        <!-- </div>
+        <div class="row g-4" style="justify-content:center"> -->
+        @include('widgets.artical_card',
+            [
+                'title'=>'Solar Project Consulting',
+                'img'=>'images/b-gallery-4.jpg',
+                'description'=>'Navigating the complexities of solar projects requires expertise, and our consulting services are
+                            here to guide you every step of the way',
+                'href'=>''
+            ])
+
+            @include('widgets.artical_card',
+            [
+                'title'=>'Solar Water Solutions',
+                'img'=>'images/b-gallery-5.jpg',
+                'description'=>'Empowering homes and businesses with eco-friendly water pumping and heating solutions powered by the sun.',
+                'href'=>''
+            ])
+         
+
         </div>
     </div>
 </div>
---
+++ resources/views/widgets/artical_card.blade.php
@@ -0,0 +1,17 @@
+
+
+<div class="col-md-7 col-lg-4 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="service-item rounded overflow-hidden">
+                    <div>
+                    <img class="img-fluid" src="{{ $img }}" style="max-height: 300px; alt="">
+                    </div>
+                    <div class="position-relative p-4 pt-0">
+                        <div class="service-icon">
+                            <i class="fa fa-solar-panel fa-3x"></i>
+                        </div>
+                        <h4 class="mb-3">{{ $title }}</h4>
+                        <p> {{ $description }}</p>
+                        <a class="small fw-medium" href="{{ $href }">Read More<i class="fa fa-arrow-right ms-2"></i></a>
+                    </div>
+                </div>
+</div>
