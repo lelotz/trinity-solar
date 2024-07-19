4c11a86be5dd8cbceefc3c0416a88b6486995336
Michael Kulaya <bmkulaya@gmail.com>
2024-07-19 13:57:27 +0300

    Removing the testimonials image elements

--- resources/views/components/testimonial.blade.php
+++ resources/views/components/testimonial.blade.php
@@ -7,42 +7,24 @@
         </div>
         <div class="owl-carousel testimonial-carousel wow fadeInUp" data-wow-delay="0.1s">
             <div class="testimonial-item text-center">
-                <div class="testimonial-img position-relative">
-                    <img class="img-fluid rounded-circle mx-auto mb-5" src="img/testimonial-1.jpg">
-                    <div class="btn-square bg-primary rounded-circle">
-                        <i class="fa fa-quote-left text-white"></i>
-                    </div>
-                </div>
                 <div class="testimonial-text text-center rounded p-4">
                     <p>Trinity Solar's services are exceptional! Their team installed our solar panels quickly and efficiently. Our energy bills have significantly reduced, and we are delighted with the results. Their professionalism and dedication are top-notch. Highly recommended for anyone considering solar energy.</p>
-                    <h5 class="mb-1">Client Name</h5>
-                    <span class="fst-italic">Profession</span>
+                    <h5 class="mb-1">Michael Kulaya</h5>
+                    <span class="fst-italic">IT Consultant</span>
                 </div>
             </div>
             <div class="testimonial-item text-center">
-                <div class="testimonial-img position-relative">
-                    <img class="img-fluid rounded-circle mx-auto mb-5" src="img/testimonial-2.jpg">
-                    <div class="btn-square bg-primary rounded-circle">
-                        <i class="fa fa-quote-left text-white"></i>
-                    </div>
-                </div>
                 <div class="testimonial-text text-center rounded p-4">
                     <p>We couldn't be happier with Trinity Solar! Their knowledgeable team guided us through the entire process, from consultation to installation. The transition to solar energy was smooth and hassle-free. We are now enjoying substantial savings on our electricity bills. A fantastic experience!</p>
-                    <h5 class="mb-1">Client Name</h5>
-                    <span class="fst-italic">Profession</span>
+                    <h5 class="mb-1">Anthony Mokiwa</h5>
+                    <span class="fst-italic">Tax Auditor</span>
                 </div>
             </div>
             <div class="testimonial-item text-center">
-                <div class="testimonial-img position-relative">
-                    <img class="img-fluid rounded-circle mx-auto mb-5" src="img/testimonial-3.jpg">
-                    <div class="btn-square bg-primary rounded-circle">
-                        <i class="fa fa-quote-left text-white"></i>
-                    </div>
-                </div>
                 <div class="testimonial-text text-center rounded p-4">
                     <p>Trinity Solar provided outstanding service from start to finish. Their experts conducted a thorough assessment of our needs and customized the perfect solar solution for us. The installation was seamless, and we are thrilled with the performance of our new system. Highly recommended!</p>
-                    <h5 class="mb-1">Client Name</h5>
-                    <span class="fst-italic">Profession</span>
+                    <h5 class="mb-1">Herbert Godson</h5>
+                    <span class="fst-italic">Lab Professional</span>
                 </div>
             </div>
         </div>
