d959ab113868f8d46fa43eceec4805a7c48bca78
Michael Kulaya <bmkulaya@gmail.com>
2024-08-04 18:00:08 +0300

    Removing Spinner

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -39,14 +39,6 @@
 <body>
 <div class="w-100 bg-white p-0">
 
-    <!-- Spinner Start -->
-    <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
-        <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
-            <span class="sr-only"></span>
-        </div>
-    </div>
-    <!-- Spinner End -->
-
     <header>
         @include('components.top_bar')
 
